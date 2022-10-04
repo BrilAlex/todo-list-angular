bf406d76cc6f5f4fb06cb7d88c65692c65f3c3c8
BrilAlex <brilalex.it@yandex.ru>
2022-10-04 13:27:56 +0300

    Error handling implemented for Todos and Tasks services

--- src/app/features/todos/services/tasks.service.ts
+++ src/app/features/todos/services/tasks.service.ts
@@ -1,64 +1,100 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {environment} from "../../../../environments/environment";
 import {DomainTasks, GetTasksResponse, Task, UpdateTaskModel} from "../models/tasks.models";
-import {BehaviorSubject, map, Observable} from "rxjs";
+import {BehaviorSubject, catchError, EMPTY, map} from "rxjs";
 import {BaseResponse} from "../../../core/models/core.models";
+import {NotificationService} from "../../../core/services/notification.service";
+import {ResultCode} from "../../../core/enum/resultCode.enum";
 
 @Injectable()
 export class TasksService {
   private tasks$ = new BehaviorSubject<DomainTasks>({});
   tasks = this.tasks$.asObservable();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private notificationService: NotificationService) {
   };
 
   getTasks(todoId: string) {
     this.http
       .get<GetTasksResponse>(`${environment.baseURL}/todo-lists/${todoId}/tasks`)
-      .pipe(map(response => response.items))
-      .subscribe((items: Task[]) => {
-        const tasks = this.tasks$.getValue();
-        tasks[todoId] = items;
-        this.tasks$.next(tasks);
-      });
+      .pipe(
+        catchError(this.errorHandler.bind(this)),
+        map((response): DomainTasks => {
+          if (!response.error) {
+            const tasks = this.tasks$.getValue();
+            tasks[todoId] = response.items;
+            return tasks;
+          } else {
+            this.notificationService.handleError(response.error);
+            return this.tasks$.getValue();
+          }
+        }),
+      )
+      .subscribe((tasks: DomainTasks) => this.tasks$.next(tasks));
   };
 
   addTask(todoId: string, taskTitle: string) {
     this.http
       .post<BaseResponse<{ item: Task }>>(`${environment.baseURL}/todo-lists/${todoId}/tasks`, {title: taskTitle})
-      .pipe(map(response => response.data.item))
-      .subscribe(newTask => {
-        const tasks = this.tasks$.getValue();
-        tasks[todoId] = [newTask, ...tasks[todoId]];
-        this.tasks$.next(tasks);
-      });
+      .pipe(
+        catchError(this.errorHandler.bind(this)),
+        map((response): DomainTasks => {
+          if (response.resultCode === ResultCode.success) {
+            const tasks = this.tasks$.getValue();
+            const newTask = response.data.item;
+            tasks[todoId] = [newTask, ...tasks[todoId]];
+            return tasks;
+          } else {
+            this.notificationService.handleError(response.messages[0]);
+            return this.tasks$.getValue();
+          }
+        }),
+      )
+      .subscribe((tasks: DomainTasks) => this.tasks$.next(tasks));
   };
 
   deleteTask(todoId: string, taskId: string) {
     this.http
       .delete<BaseResponse>(`${environment.baseURL}/todo-lists/${todoId}/tasks/${taskId}`)
       .pipe(
-        map(() => {
-          const tasks = this.tasks$.getValue();
-          tasks[todoId] = tasks[todoId].filter(t => t.id !== taskId);
-          return tasks;
-        })
+        catchError(this.errorHandler.bind(this)),
+        map((response): DomainTasks => {
+          if (response.resultCode === ResultCode.success) {
+            const tasks = this.tasks$.getValue();
+            tasks[todoId] = tasks[todoId].filter(t => t.id !== taskId);
+            return tasks;
+          } else {
+            this.notificationService.handleError(response.messages[0]);
+            return this.tasks$.getValue();
+          }
+        }),
       )
-      .subscribe((tasks) => this.tasks$.next(tasks));
+      .subscribe((tasks: DomainTasks) => this.tasks$.next(tasks));
   };
 
   updateTask(todoId: string, taskId: string, model: UpdateTaskModel) {
     this.http
       .put<BaseResponse<{ item: Task }>>(`${environment.baseURL}/todo-lists/${todoId}/tasks/${taskId}`, model)
       .pipe(
-        map(response => {
-          const tasks = this.tasks$.getValue();
-          const updatedTask = response.data.item;
-          tasks[todoId] = tasks[todoId].map(t => t.id === taskId ? updatedTask : t);
-          return tasks;
-        })
+        catchError(this.errorHandler.bind(this)),
+        map((response): DomainTasks => {
+          if (response.resultCode === ResultCode.success) {
+            const tasks = this.tasks$.getValue();
+            const updatedTask = response.data.item;
+            tasks[todoId] = tasks[todoId].map(t => t.id === taskId ? updatedTask : t);
+            return tasks;
+          } else {
+            this.notificationService.handleError(response.messages[0]);
+            return this.tasks$.getValue();
+          }
+        }),
       )
-      .subscribe((tasks) => this.tasks$.next(tasks));
+      .subscribe((tasks: DomainTasks) => this.tasks$.next(tasks));
+  };
+
+  private errorHandler(error: HttpErrorResponse) {
+    this.notificationService.handleError(error.message);
+    return EMPTY;
   };
 }
--- src/app/features/todos/services/todos.service.ts
+++ src/app/features/todos/services/todos.service.ts
@@ -1,25 +1,28 @@
 import {Injectable} from "@angular/core";
-import {HttpClient} from "@angular/common/http";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {environment} from "../../../../environments/environment";
 import {DomainTodo, FilterValue, Todo} from "../models/todos.models";
-import {BehaviorSubject, map} from "rxjs";
+import {BehaviorSubject, catchError, EMPTY, map} from "rxjs";
 import {BaseResponse} from "../../../core/models/core.models";
+import {NotificationService} from "../../../core/services/notification.service";
+import {ResultCode} from "../../../core/enum/resultCode.enum";
 
 @Injectable()
 export class TodosService {
   private todos$ = new BehaviorSubject<DomainTodo[]>([]);
   todos = this.todos$.asObservable();
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private notificationService: NotificationService) {
   };
 
   getTodos() {
     this.http
       .get<Todo[]>(`${environment.baseURL}/todo-lists`)
       .pipe(
+        catchError(this.errorHandler.bind(this)),
         map((response): DomainTodo[] => {
           return response.map(tdl => ({...tdl, filter: "all"}));
-        })
+        }),
       )
       .subscribe((todos: DomainTodo[]) => this.todos$.next(todos));
   };
@@ -28,10 +31,16 @@ export class TodosService {
     this.http
       .post<BaseResponse<{ item: Todo }>>(`${environment.baseURL}/todo-lists`, {title})
       .pipe(
-        map((response): DomainTodo[] => {
-          const newTodo: DomainTodo = {...response.data.item, filter: "all"};
-          return [newTodo, ...this.todos$.getValue()];
-        })
+        catchError(this.errorHandler.bind(this)),
+        map((response) => {
+          if (response.resultCode === ResultCode.success) {
+            const newTodo: DomainTodo = {...response.data.item, filter: "all"};
+            return [newTodo, ...this.todos$.getValue()];
+          } else {
+            this.notificationService.handleError(response.messages[0]);
+            return this.todos$.getValue();
+          }
+        }),
       )
       .subscribe((todos: DomainTodo[]) => this.todos$.next(todos));
   };
@@ -40,7 +49,15 @@ export class TodosService {
     this.http
       .delete<BaseResponse>(`${environment.baseURL}/todo-lists/${id}`)
       .pipe(
-        map((): DomainTodo[] => this.todos$.getValue().filter(tdl => tdl.id !== id))
+        catchError(this.errorHandler.bind(this)),
+        map((response): DomainTodo[] => {
+          if (response.resultCode === ResultCode.success) {
+            return this.todos$.getValue().filter(tdl => tdl.id !== id);
+          } else {
+            this.notificationService.handleError(response.messages[0]);
+            return this.todos$.getValue();
+          }
+        }),
       )
       .subscribe((todos: DomainTodo[]) => this.todos$.next(todos));
   };
@@ -49,12 +66,25 @@ export class TodosService {
     this.http
       .put<BaseResponse>(`${environment.baseURL}/todo-lists/${id}`, {title})
       .pipe(
-        map((): DomainTodo[] => this.todos$.getValue().map((tdl => tdl.id === id ? {...tdl, title} : tdl)))
+        catchError(this.errorHandler.bind(this)),
+        map((response): DomainTodo[] => {
+          if (response.resultCode === ResultCode.success) {
+            return this.todos$.getValue().map(tdl => tdl.id === id ? {...tdl, title} : tdl);
+          } else {
+            this.notificationService.handleError(response.messages[0]);
+            return this.todos$.getValue();
+          }
+        }),
       )
       .subscribe((todos: DomainTodo[]) => this.todos$.next(todos));
   };
 
   changeTodoFilter(id: string, filter: FilterValue) {
     this.todos$.next(this.todos$.getValue().map(tdl => tdl.id === id ? {...tdl, filter} : tdl));
-  }
+  };
+
+  private errorHandler(error: HttpErrorResponse) {
+    this.notificationService.handleError(error.message);
+    return EMPTY;
+  };
 }
